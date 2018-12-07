1a6eeeab4c0b02b7abc318c8e06c9f81cb35045b
vagrant <vagrant@vagrant.vm>
2018-12-07 13:51:43 +0000

    First commit

---
+++ LogsAnalysis.py
@@ -0,0 +1,91 @@
+import psycopg2
+
+# Connect to database and setup cursor
+connection = psycopg2.connect("dbname=news")
+cursor = connection.cursor()
+
+# The most popular article authors of all time
+
+
+def popularArticle():
+    query = """
+            SELECT articles.title,
+                   count(*)
+            FROM   log,
+                   articles
+            WHERE  log.path = '/article/' || articles.slug
+            GROUP BY articles.title
+            ORDER BY count(*) DESC
+            LIMIT 3;
+    """
+    cursor.execute(query)
+    return cursor.fetchall()
+    db.close()
+
+# The most popular article authors of all time
+
+
+def popularAuthors():
+    query = """
+            SELECT authors.name,
+                   count(*)
+            FROM   log,
+                   articles,
+                   authors
+            WHERE  log.path = '/article/' || articles.slug
+              AND articles.author = authors.id
+            GROUP BY authors.name
+            ORDER BY count(*) DESC
+            LIMIT 3;
+    """
+    cursor.execute(query)
+    return cursor.fetchall()
+    db.close()
+
+# On which days did more than 1% of requests lead to errors
+
+
+def errorsRequests():
+    query = """
+            WITH num_requests AS (
+                SELECT time::date AS day, count(*)
+                FROM log
+                GROUP BY time::date
+                ORDER BY time::date
+              ), num_errors AS (
+                SELECT time::date AS day, count(*)
+                FROM log
+                WHERE status != '200 OK'
+                GROUP BY time::date
+                ORDER BY time::date
+              ), error_rate AS (
+                SELECT num_requests.day,
+                  num_errors.count::float / num_requests.count::float * 100
+                  AS error_pc
+                FROM num_requests, num_errors
+                WHERE num_requests.day = num_errors.day
+              )
+            SELECT * FROM error_rate WHERE error_pc > 1;
+    """
+    cursor.execute(query)
+    return cursor.fetchall()
+    db.close()
+
+# Print out put
+
+result01 = popularArticle()
+result02 = popularAuthors()
+result03 = errorsRequests()
+
+print(
+    'The most three popular articles:')
+for(title, count) in result01:
+    print(" {} - {} views".format(title.replace('-', ' ').capitalize(), count))
+
+print(
+    '\nThe most popular author:\n' +
+    ' %s - %d views\n' % (result02[0][0], result02[0][1]))
+
+print(
+    'The days with error rate higher than 1'+'%:\n' + ' %s - %0.1f'
+    % (result03[0][0].strftime('%B %d, %Y'), result03[0][1])+'%\n')
---
+++ README.md
@@ -0,0 +1,22 @@
+#A Udacity FSND project.
+
+
+## logs analysis project
+It's an internal reporting tool that will use information from the database to discover what kind of articles the site's readers like.
+
+The database contains newspaper articles, as well as the web server log for the site. The log has a database row for each time a reader loaded a web page. Using that information, your code will answer questions about the site's user activity.
+
+The program in this project will run from the command line. It won't take any input from the user. Instead, it will connect to that database, use SQL queries to analyze the log data, and print out the answers to some questions.
+
+## What you need to RUN?
+1/ Vagrant:	https://www.vagrantup.com/downloads.htmlb.VirtualMachine
+2/ VirtualMachine:	https://www.virtualbox.org/wiki/Downloadsc.Download
+3/ Download	a	FSND	VirtualMachine https://github.com/udacity/fullstack-nanodegree-vm
+4/ Python https://www.python.org
+
+## How to RUN the Project
+```
+cd / vagrant
+cd log-analysis-project
+python3 LogsAnalysis.py
+```
